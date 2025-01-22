46bd49e008685fba7ed3b6968d88237d0f5bc894
UrePu <darkin31313@gmail.com>
2025-01-22 20:56:36 +0900

    a

--- part2/02.배열/(lv.2)문자열-뒤집기.js
+++ part2/02.배열/(lv.2)문자열-뒤집기.js
@@ -9,7 +9,22 @@
  * @returns {string}
  */
 
-function reverseString(str) {}
+function reverseString(str) {
+  // 문자열이 아닌 경우 빈 문자열 반환
+  if (typeof str !== "string") {
+    return "";
+  }
+
+  // 문자열을 뒤집기 위해 결과를 저장할 변수
+  let reversed = "";
+
+  // 반복문을 사용하여 문자열 뒤집기
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
 
 // export 를 수정하지 마세요.
 export { reverseString };
