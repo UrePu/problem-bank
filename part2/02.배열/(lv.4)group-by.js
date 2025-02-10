c4c2a50ee67f491902649ca6a4e9b5b8ec9092eb
UrePu <darkin31313@gmail.com>
2025-02-10 20:37:49 +0900

    4

--- part2/02.배열/(lv.4)group-by.js
+++ part2/02.배열/(lv.4)group-by.js
@@ -28,7 +28,15 @@
  */
 
 // TODO: groupBy 함수를 작성하세요.
-function groupBy(arr, key) {}
+function groupBy(arr, key) {
+  return arr.reduce((acc, cur) => {
+    if (!acc[cur[key]]) {
+      acc[cur[key]] = [];
+    }
+    acc[cur[key]].push(cur);
+    return acc;
+  }, {});
+}
 
 // export를 수정하지 마세요.
 export { groupBy };
